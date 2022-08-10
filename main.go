87e352419598220949682e4590e1f565bdf0f993
Nabeel Saabna <15704531+nabeelys@users.noreply.github.com>
2022-08-10 18:40:15 +0300

    Create main.go

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	libs "gitlab.com/nabeelsaabna/go-private-nabeel"
+)
+
+func Sum(a, b int) int {
+	return lib.Multiply(a, b)
+}
+
+func PrintSum(a, b int) {
+	libs.PrintSum(a, b)
+}
